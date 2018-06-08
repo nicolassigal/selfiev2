6325198f538cfb425d46ea94901971990c6d4ce3
nicolas sigal <nicolas.sigal@gmail.com>
2018-06-07 21:04:31 -0300

    asasd

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,39 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
+import { WebWorkerService } from 'angular2-web-worker';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
+  providers: [WebWorkerService],
   styleUrls: ['./shipping.component.scss']
 })
 export class ShippingComponent implements OnInit {
   data: any;
-  constructor() { }
+  results = new Subject<any>();
+  constructor(private _worker: WebWorkerService) { }
 
   ngOnInit() {
+    this.results.subscribe(wb => this.parseXLSX(wb));
   }
 
   onFileChange(evt: any) {
-    /* wire up file reader */
     const target: DataTransfer = <DataTransfer>(evt.target);
-    if (target.files.length !== 1) {
-      throw new Error('Cannot use multiple files');
-    }
     const reader: FileReader = new FileReader();
     reader.onload = (e: any) => {
-      /* read workbook */
+      console.log('reader load file');
       const bstr: string = e.target.result;
-      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
-
-      /* grab first sheet */
-      const wsname: string = wb.SheetNames[0];
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-      /* save data */
-      this.data = XLSX.utils.sheet_to_json(ws, { defval: null, blankrows: false});
-    console.log(this.data);
-
+      this._worker.run(this.readXLSX, bstr);
     };
     reader.readAsBinaryString(target.files[0]);
+  }
+
+  readXLSX = (bstr) => {
+    console.log('reading');
+    const wb = XLSX.read(bstr, {type: 'binary'});
+    console.log('wb', wb);
+    this.results.next(wb);
+  }
+
+  parseXLSX = (wb) => {
+    console.log('parsing');
+    const wsname: string = wb.SheetNames[0];
+    const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+    this.data = XLSX.utils.sheet_to_json(ws, { defval: null, blankrows: false});
+    console.log(this.data);
   }
 }
