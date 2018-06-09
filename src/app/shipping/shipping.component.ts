862b5bb6436cbbe2e43e94762ab0e625955489f9
nicolas sigal <nicolas.sigal@gmail.com>
2018-06-09 12:00:27 -0300

    'asdas'

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -4,6 +4,7 @@ import * as XLSX from 'xlsx';
 import { WorkersService } from '../workers.service';
 import { InfoService } from '../info/info.service';
 import { AngularFirestore } from 'angularfire2/firestore';
+import { resetFakeAsyncZone } from '@angular/core/testing';
 
 @Component({
   selector: 'app-shipping',
@@ -13,11 +14,12 @@ import { AngularFirestore } from 'angularfire2/firestore';
 export class ShippingComponent implements OnInit {
   data: any;
   results = new Subject<any>();
+  dataToBeAdded;
+  dataToBeUpdated;
   xlsxWorker = `self.addEventListener("message", (e) => {
     if(e.data.msg === "Start Worker") {
         importScripts(e.data.url + '/xlsx/xlsx.full.min.js');
         let wb = XLSX.read(e.data.bstr, {type: 'binary'});
-        console.log('wb', wb);
         let wsname = wb.SheetNames[0];
         let ws = wb.Sheets[wsname];
         self.postMessage(JSON.stringify(XLSX.utils.sheet_to_json(ws, { defval: null, blankrows: false})));
@@ -30,8 +32,17 @@ export class ShippingComponent implements OnInit {
 
  checkDBWorker = `self.addEventListener("message", (e) => {
   if(e.data.msg === "Start Worker") {
-      console.log('data from checkdbWorker', e.data.xlsxData);
-      self.postMessage(JSON.stringify(e.data.xlsxData));
+      let toStorage;
+      let xls = e.data.xlsData;
+      let db = e.data.dbData;
+      console.log('db length', db.length);
+      if (db.length) {
+        toStorage = xls.filter(row => !db.some(entry => row.hbr_id === entry.hbr_id));
+      } else {
+        toStorage = xls;
+      }
+      console.log('to storage', toStorage);
+      self.postMessage(JSON.stringify(toStorage));
   }
   if(e.data.msg === "Stop Worker") {
       self.removeEventListener("message");
@@ -44,14 +55,12 @@ export class ShippingComponent implements OnInit {
     private _db: AngularFirestore) {}
 
   ngOnInit() {
-    const data = JSON.parse(sessionStorage.getItem('data'));
-    if (data) {
+    this._db.collection('operations')
+    .valueChanges()
+    .subscribe(data => {
       this.data = data;
-    }
-  }
-
-  clearData = () => {
-    sessionStorage.clear();
+      console.log('new data!');
+    });
   }
 
   parseXLS = (evt: any) => {
@@ -66,23 +75,49 @@ export class ShippingComponent implements OnInit {
         this.infoService.showMessage('<p> Processing... please wait </p>');
         this._worker.worker.addEventListener('message', (response) => {
           this._worker.terminateWorker();
-          this.data = JSON.parse(response.data);
-          this.saveData(this.data);
+          this.prepareData(this.data, JSON.parse(response.data));
         });
       };
       reader.readAsBinaryString(target.files[0]);
     }
   }
 
-  saveData = (data) => {
-    this.infoService.showMessage('<p> Checking with database... </p>');
+  prepareData = (data, xls) => {
+    this.infoService.showMessage('<p> Preparing Data... </p>');
     this._worker.createWorker(this.checkDBWorker);
-    const msgToWorker = { msg: 'Start Worker', xlsxData: data };
+    const msgToWorker = { msg: 'Start Worker', xlsData: xls, dbData: data };
     this._worker.postMessageToWorker(msgToWorker);
     this._worker.worker.addEventListener('message', (response) => {
-      console.log(response.data);
-      this.infoService.showMessage('<p> done!! </p>');
-      setTimeout(() => { this.infoService.hideMessage(); }, 3000);
+      this._worker.terminateWorker();
+      const result = JSON.parse(response.data);
+      if (result.length) {
+        this.addEntry(result);
+      } else {
+        this.infoService.showMessage('<p> Nothing to update. </p>');
+        setTimeout(this.finishProccesing, 3000);
+      }
     });
+  }
+
+  addEntry = (data) => {
+    this.infoService.showMessage(`<p> Updating database with ${data.length} new entries </p>'`);
+    const promiseArr = [];
+    data.map(entry => {
+      promiseArr.push(this._db.collection('operations').add(entry));
+    });
+
+    Promise.all(promiseArr)
+      .then(res => console.log(res))
+      .then(() => this.finishProccesing())
+      .catch(res => console.log(res));
+  }
+
+  updateEntry = () => {
+
+  }
+
+  finishProccesing = () => {
+    this.infoService.showMessage('<p> Done </p>');
+    setTimeout(this.infoService.hideMessage, 3000);
   }
 }
